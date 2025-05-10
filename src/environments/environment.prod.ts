0ba4e423e38d0792bf0cd6c295c1eaddbc1cc6ba
DivergenteNM <nicolasmdivergent@gmail.com>
2025-05-10 16:45:08 -0500

    feat(env): add environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://mitienda.com/api',
+    keycloak: {
+      url: 'https://auth.mitienda.com',
+      realm: 'ecommerce',
+      clientId: 'angular-client'
+    }
+  };
+  
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api',
+    keycloak: {
+      url: 'http://localhost:8080',
+      realm: 'ecommerce',
+      clientId: 'angular-client'
+    }
+  };
+  
