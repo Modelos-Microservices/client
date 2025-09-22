0625a0b84fa44810aa4a63eb256607b1c5881cd6
DivergenteNM <nicolasmdivergent@gmail.com>
2025-09-22 17:14:46 -0500

    fix(env): fix hte dashboard number

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,8 +10,8 @@ export const environment = {
       url: 'http://localhost:3100',
       siteUrl: 'http://localhost:3100',
       token: '615832aa47ebef5bb1604242071472cfe9e4c5b1ec0383d44661606a6e18063c',
-      dashboardId: 1, // Replace with your actual dashboard ID
-      cardId: 1 // Replace with your actual card ID 
+      dashboardId: 98, 
+      cardId: 1 
     }
   };
   
